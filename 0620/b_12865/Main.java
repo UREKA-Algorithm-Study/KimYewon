84884e410d045c71a40b67abbd344dc8bc181d37
kimyewon <yaewon46438@gmail.com>
2024-06-21 00:34:10 +0900

    Add Baekjoon 12865 file with solution (0620)

--- 0620/b_12865/Main.java
+++ 0620/b_12865/Main.java
@@ -6,33 +6,35 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt(); // 물품 수 
-		int K = sc.nextInt(); // 최대 무게 
+		int N = sc.nextInt(); // 물품 수 (4)
+		int K = sc.nextInt(); // 최대 무게 (7)
 		int[][] dp = new int[N+1][K+1];
-		int[][] wv = new int[N][2];
+		int[] w = new int[N+1]; // 각 물건의 무게 
+		int[] v = new int[N+1]; // 각 물건의 가치 
 		
-		// [ [6,13], [4,8], [3,6], [5,12] ]
-		for (int i=0; i<N; i++) {
-			wv[i][0] = sc.nextInt();
-			wv[i][1] = sc.nextInt();
+		// w = [0, 6, 4, 3, 5] / v = [0, 13, 8, 6, 12]
+		for (int i=1; i<N+1; i++) {
+			w[i] = sc.nextInt();
+			v[i] = sc.nextInt();
 		}
 		
-		for (int i=0; i<K+1; i++) {
-			if (i>=wv[0][0]) {
-				dp[0][i] = wv[0][1];
+		for (int i=0; i<K+1; i++) { // i =1 i= 7
+			if (i>=w[1]) { // i >= 6 => 6, 7
+				dp[1][i] = v[1];
 			}
 		}
 		
-		for (int i=1; i<N; i++) {
-			for (int j=0; j<K+1; j++) {
-				if (j < wv[i][0]) {
+		for (int i=2; i<N+1; i++) {
+			for (int j=0; j<K+1; j++) { // i=3 / j=7
+				if (j < w[i]) { // 7 < 3
 					dp[i][j] = dp[i-1][j];
 				} else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-wv[i][0]]+wv[i][1]);
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]);
 				}
 			}
 		}
-		System.out.println(dp[N-1][K]);
+		System.out.println(dp[N][K]);
 		sc.close();
 	}
+
 }
